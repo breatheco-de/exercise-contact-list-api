c5a45153a68b282b82a278574cf556d3169b1fe6
Alejandro Sanchez <aalejo@gmail.com>
2020-05-12 12:23:44 -0400

    removed django

--- django/contact_list/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from api import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('contact/<str:contact_name>', views.SingleContactView.as_view(), name='contact'),
-    path('contact/', views.ContactView.as_view(), name='contact'),
-]
